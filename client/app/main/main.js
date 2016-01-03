bfc8269ee57c3c58a6e7087662ae5d74ad77ebbe
Andrew Kinley <adkinley@gmail.com>
2016-01-02 19:48:02 -0500

    Removed main.html template

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,10 +1,4 @@
 'use strict';
 
 angular.module('kinleyParserApp')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
-  });
+ );
